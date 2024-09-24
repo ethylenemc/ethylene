b8ab15b0cecd44e18dd3891729bc5dce94d9a35d
TonimatasDEV <tonimatasdev@proton.me>
2024-09-24 19:51:52 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("fabric-loom") version "1.7-SNAPSHOT"
+}
+
+val modVersion: String by extra
+val minecraftVersion: String by extra
+val loaderVersion: String by extra
+val yarnVersion: String by extra
+
+version = modVersion
+group = "dev.tonimatas"
+
+base.archivesName.set("ethylene")
+
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(21)
+    withSourcesJar()
+}
+
+dependencies {
+    minecraft("com.mojang:minecraft:$minecraftVersion")
+    mappings("net.fabricmc:yarn:$yarnVersion:v2")
+    modImplementation("net.fabricmc:fabric-loader:$loaderVersion")
+
+    //modImplementation "net.fabricmc.fabric-api:fabric-api:${project.fabric_version}"
+}
+
+tasks.processResources {
+    val properties = mapOf("version" to modVersion, "minecraftVersion" to minecraftVersion, "loaderVersion" to loaderVersion)
+    inputs.properties(properties)
+    
+    filesMatching("fabric.mod.json") {
+        expand(properties)
+    }
+}
---
+++ src/main/java/dev/tonimatas/ethylene/Ethylene.java
@@ -0,0 +1,10 @@
+package dev.tonimatas.ethylene;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Ethylene implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
